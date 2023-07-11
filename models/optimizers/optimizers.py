1977208509a999eec9e1304a4c30a2d8f91a7508
root <rafi.zon@hotmail.com>
2023-07-11 19:50:24 +0000

    colab

--- models/optimizers/optimizers.py
+++ models/optimizers/optimizers.py
@@ -4,8 +4,9 @@ from ..schedulers.lr_scheduler import *
 
 def get_optimizer(CFG):
     if CFG['optimizer']['name'] == 'adam':
-        opt = tf.keras.optimizers.Adam(clipvalue = CFG['optimizer']['clipvalue'], clipnorm = CFG['optimizer']['clipnorm']) # .legacy
-    
+#        opt = tf.keras.optimizers.Adam(clipvalue = CFG['optimizer']['clipvalue'], clipnorm = CFG['optimizer']['clipnorm']) # .legacy
+         opt = tf.keras.optimizers.Adam()
+         
     elif CFG['optimizer']['name'] == 'radam':
         if CFG['scheduler']['name'] == 'one_cycle':
             schedule, decay_schedule = get_one_cycle_scheduler(CFG)
